683fb8dc18697b29f13dba736a6fc4695b90b819
AntiPrim3 <amit.rockinad.das@gmail.com>
2022-06-25 13:35:37 +0530

    start: Initial commit [Otaku]

---
+++ README.md
@@ -0,0 +1,11 @@
+# A Node CLI Utility to create Vanilla Webpage
+
+## Usage:
+
+```
+wcreate <appname>
+cd <appname>
+npm install || yarn install
+```
+
+> This comes with pre-configured tailwind for your project.
---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+const { program } = require("commander");
+const { copySync } = require("fs-extra");
+
+program.argument("<appname>", "Enter name of you app").action((appname) => {
+  copySync("./project", `${appname}`);
+});
+
+program.parse();
